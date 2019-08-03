4cb8f45a34f96eec85e12ccb59612dcaa214ec8a
singhmahabir <msdeo4u@hotmail.com>
2019-08-03 23:51:56 +0530

    data sql changed and profiles modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into User
-values(hibernate_sequence.NEXTVAL,'1234','Good book for java ',4, 'msdeo4u');
+values(hibernate_sequence.NEXTVAL,'1234','Good book for java ',4, 'msdeo');
 
 insert into User
-values(hibernate_sequence.NEXTVAL,'2345','Average book ',3, 'msdeo4u');
+values(hibernate_sequence.NEXTVAL,'2345','Average book ',3, 'msdeo');
